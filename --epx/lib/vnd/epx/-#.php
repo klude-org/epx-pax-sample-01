00f1a85c30f2a7260d9ee86799e3a3ffa9667d50
klude-au <124860775+klude-au@users.noreply.github.com>
2026-01-05 19:55:06 +1030

    update

--- --epx/lib/vnd/epx/-#.php
+++ --epx/lib/vnd/epx/-#.php
@@ -7,15 +7,15 @@ final class epx {
     public readonly string $SPLEX_DIR;
     private $TSP_LIST = [];
     
-    public static function _(...$args) { static $i;  return $i ?: ($i = new static(...$args)); }
+    public static function _() {  static $i;  return $i ?? ($i = new static);  }
     
-    private function __construct(){
-        $this->SITE_DIR = ($_SERVER['FW__SITE_DIR'] = \str_replace('\\','/', empty($_SERVER['HTTP_HOST'])
-            ? \realpath($_SERVER['FW__SITE_DIR'] ?? \getcwd())
-            : \realpath(\dirname($_SERVER['SCRIPT_FILENAME'])) 
-        ));
-        $this->PPLEX_DIR = \str_replace('\\','/', \dirname(__DIR__,2));
-        $this->SPLEX_DIR = "{$this->SITE_DIR}/--epx";
+    public function __construct(){
+        $this->SITE_DIR = $_SERVER['_']['SITE_DIR'];
+        $this->PPLEX_DIR = $_SERVER['_']['START_DIR'];
+        $this->SPLEX_DIR = "{$_SERVER['_']['SITE_DIR']}/--epx";
+        foreach(\explode(PATH_SEPARATOR, \get_include_path()) as $dir){
+            $this->TSP_LIST[\strtr($dir, '\\','/')] = true;
+        }
     }
     
     private function resolve($path){
@@ -33,27 +33,30 @@ final class epx {
     public function module(string $path, string $source = null, string $version = 'main'){
         if(($path[0]??'')=='/' || ($path[1]??'')==':'){
             if(\is_dir($path)){
-                $this->TSP_LIST[\strtr($path, '\\','/')] = true;
+                $this->TSP_LIST = [\strtr($path, '\\','/') => true] + $this->TSP_LIST;
             }
         } else if($source) {
             $p = \strtr($path.'-('.$source.'~'.$version.')','/','~');
             switch(\strtok($source, '/')){
                 case 'github':{
                     if($d  = $this->resolve($p)){
-                        $this->TSP_LIST[\strtr($d, '\\','/')] = true;
+                        $this->TSP_LIST = [\strtr($d, '\\','/') => true] + $this->TSP_LIST;
                     } else if(
                         \class_exists($c = \epx\module_installer\github::class)
-                        && ($c::_()($d = "{$this->PPLEX_DIR}/.local/{$p}", $path, $source,$version))
+                        && ($c::_()($d = "{$this->PPLEX_DIR}/.local/{$p}", $path, $source, $version))
                     ){
-                        $this->TSP_LIST[\strtr($d, '\\','/')] = true;
+                        $this->TSP_LIST = [\strtr($d, '\\','/') => true] + $this->TSP_LIST;
                     }
                 } break;
             }
+        } else {
+            if($d  = $this->resolve($path)){
+                $this->TSP_LIST = [\strtr($d, '\\','/') => true] + $this->TSP_LIST;
+            }
         }
+        \set_include_path(\implode(PATH_SEPARATOR, \array_keys(\array_filter($this->TSP_LIST))));
         return $this;
     }
     
-    public function execute(){
-        
-    }
+    public function execute(){ }
 }
